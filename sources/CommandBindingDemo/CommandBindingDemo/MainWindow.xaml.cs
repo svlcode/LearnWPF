05589cf044dff96fba2536f9dffca279249c78ca
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2017-05-02 11:22:30 +0300

    Added CommandBindingDemo project

---
+++ sources/CommandBindingDemo/CommandBindingDemo/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandBindingDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CommandBinding copyBinding = new CommandBinding(ApplicationCommands.Copy);
+            CommandBinding pasteBinding = new CommandBinding(ApplicationCommands.Paste);
+
+            CommandBindings.Add(copyBinding);
+            CommandBindings.Add(pasteBinding);
+
+            copyBinding.Executed += Binding_Executed;
+            pasteBinding.Executed += Binding_Executed;
+        }
+
+        private void Binding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Debug.WriteLine("Command executed");
+        }
+    }
+}
