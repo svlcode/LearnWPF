fb363cfc6e7d32abdf4f31ba27ab62ac79e5a817
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2017-05-12 13:48:00 +0300

    Added DataTemplateDemo project

---
+++ sources/DataTemplateDemo/DataTemplateDemo/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace DataTemplateDemo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
