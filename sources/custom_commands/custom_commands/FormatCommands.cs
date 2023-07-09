fd61f0e1cb0dd3ac166bd5c23a47f64694c3b935
Vio <vio.slaniceanu@gmail.com>
2023-07-09 15:24:19 +0300

    add custom_commands

---
+++ sources/custom_commands/custom_commands/FormatCommands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace custom_commands
+{
+    public static class FormatCommands
+    {
+        static RoutedUICommand setBackground = new RoutedUICommand("Set the Background", "SetBackground", typeof(FormatCommands));
+
+        public static RoutedUICommand SetBackground { get { return setBackground; } }
+    }
+}
---
+++ sources/custom_commands/custom_commands/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace custom_commands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //MessageBox.Show("command executed");
+            RichTextBox rtb = sender as RichTextBox;
+            rtb.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.YellowGreen);
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            RichTextBox rtb = sender as RichTextBox;
+            if (rtb.Selection.IsEmpty)
+                e.CanExecute = false;
+            else
+                e.CanExecute = true;
+        }
+    }
+}
